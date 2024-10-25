ca17a3570c429afedcb20f77f4d228955e28e4f0
FSD7-Boy <boyzasad01@gmail.com>
2024-10-25 14:33:16 +0700

    React Data Fetching Find a Book Assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,54 @@
 import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [inputValue, setInputValue] = useState("");
+  const [data, setData] = useState(null);
+  const [debounceTimeout, setDebounceTimeout] = useState(null);
+
+  const asynceFunction = async (query) => {
+    try {
+      const response = await axios.get(
+        `https://openlibrary.org/search.json?q=${query}`
+      );
+      setData(response.data); 
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+
+    if (debounceTimeout) {
+      clearTimeout(debounceTimeout);
+    }
+
+    const newTimeout = setTimeout(() => {
+      asynceFunction(e.target.value); 
+    }, 500);
+
+    setDebounceTimeout(newTimeout);
+  };
+
+  return (
+    <>
+      <div className="App">
+        <h1>Find a Book</h1>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        {data ? (
+        <ul>
+          {data.docs.map((doc) => (
+            <li key={doc.key}>{doc.title}</li>
+          ))}
+        </ul>
+        ) : (
+          <p>No data available</p>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default App;
